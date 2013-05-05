d7f447832df22536fb9c6f430d7c548ac453db74
cakemix <cakemix@phenom-ubuntu>
2013-05-05 17:49:07 +0100

    Updated UI to make it easyer for me to add elements later
    Separated classes (separating display from gubbins)
    
    Added
    -Close and Create new Connection
    -Hiding of user list
    
    Changed
    -Layout of chat and userlist

--- src/org/cakemix/chat/ChatClient.java
+++ src/org/cakemix/chat/ChatClient.java
@@ -4,28 +4,125 @@ import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 import org.cakemix.chat.Network.ChatMessage;
 import org.cakemix.chat.Network.RegisterName;
 import org.cakemix.chat.Network.UpdateNames;
 
-public class ChatClient {
+public final class ChatClient {
 
     ChatFrame chatFrame;
     Client client;
+    String host, port;
     String name;
 
     public ChatClient() {
         client = new Client();
+
+        //setup the client settings
+        setupClient(client);
+
+        //popup get connection details
+        // sets host and name
+        getConnectionDetails();
+
+        // All the ugly Swing stuff is hidden in ChatFrame so it doesn't clutter the KryoNet example code.
+        chatFrame = new ChatFrame(host);
+
+        
+
+        setChatListeners();
+        chatFrame.setVisible(true);
+
+        connect();
+
+    }
+
+    protected void setChatListeners(){
+        //this is called when new connection button is clicked
+        chatFrame.createNewConnection(new Runnable() {
+            public void run() {
+                // close and stop the current
+                // client
+                client.close();
+                client.stop();
+                //create a new client
+                client = new Client();
+                //set up the new client
+                setupClient(client);
+                //get the connection details
+                //and connect
+                getConnectionDetails();
+                connect();
+            }
+        });
+        // This listener is called when the send button is clicked.
+        chatFrame.setSendListener(new Runnable() {
+            public void run() {
+                ChatMessage chatMessage = new ChatMessage();
+                chatMessage.text = chatFrame.getSendText();
+                client.sendTCP(chatMessage);
+            }
+        });
+        // This listener is called when the chat window is closed.
+        chatFrame.setCloseListener(new Runnable() {
+            public void run() {
+                client.stop();
+            }
+        });
+    }
+    
+    protected void connect() {
+        // We'll do the connect on a new thread so the ChatFrame can show a progress bar.
+        // Connecting to localhost is usually so fast you won't see the progress bar.
+        new Thread("Connect") {
+            public void run() {
+                try {
+                    client.connect(5000, host, Network.port);
+                    // Server communication after connection can go here, or in Listener#connected().
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }.start();
+    }
+
+    protected void getConnectionDetails() {
+        // Request the host from the user.
+        String input = (String) JOptionPane.showInputDialog(null, "Server Address:",
+                "Connect to chat server", JOptionPane.QUESTION_MESSAGE,
+                null, null,
+                "localhost");
+        if (input == null || input.trim().length() == 0) {
+            System.exit(1);
+        }
+        host = input.trim();
+
+        input = (String) JOptionPane.showInputDialog(null, "Server Port:",
+                "Connect to chat Server", JOptionPane.QUESTION_MESSAGE, null,
+                null, "5000");
+        if (input == null || input.trim().length() == 0) {
+            System.exit(1);
+        }
+        port = input.trim();
+
+        // Request the user's name.
+        input = (String) JOptionPane.showInputDialog(null, "Display Name:",
+                "Connect to chat server", JOptionPane.QUESTION_MESSAGE, null,
+                null, "Test");
+        if (input == null || input.trim().length() == 0) {
+            System.exit(1);
+        }
+        name = input.trim();
+    }
+
+    /*
+     * 
+     * Setup a kryo client
+     */
+    protected void setupClient(final Client client) {
         client.start();
 
         // For consistency, the classes to be sent over the network are
@@ -33,7 +130,6 @@ public class ChatClient {
         Network.register(client);
 
         client.addListener(new Listener() {
-
             public void connected(Connection connection) {
                 RegisterName registerName = new RegisterName();
                 registerName.name = name;
@@ -56,204 +152,12 @@ public class ChatClient {
 
             public void disconnected(Connection connection) {
                 EventQueue.invokeLater(new Runnable() {
-
                     public void run() {
                         // Closing the frame calls the close listener which will stop the client's update thread.
-                        chatFrame.dispose();
+                        client.close();
                     }
                 });
             }
-        });// end of registering
-
-        // Request the host from the user.
-        String input = (String) JOptionPane.showInputDialog(null, "Host:", "Connect to chat server", JOptionPane.QUESTION_MESSAGE,
-                null, null, "localhost");
-        if (input == null || input.trim().length() == 0) {
-            System.exit(1);
-        }
-        final String host = input.trim();
-
-        // Request the user's name.
-        input = (String) JOptionPane.showInputDialog(null, "Name:", "Connect to chat server", JOptionPane.QUESTION_MESSAGE, null,
-                null, "Test");
-        if (input == null || input.trim().length() == 0) {
-            System.exit(1);
-        }
-        name = input.trim();
-
-        // All the ugly Swing stuff is hidden in ChatFrame so it doesn't clutter the KryoNet example code.
-        chatFrame = new ChatFrame(host);
-        // This listener is called when the send button is clicked.
-        chatFrame.setSendListener(new Runnable() {
-
-            public void run() {
-                ChatMessage chatMessage = new ChatMessage();
-                chatMessage.text = chatFrame.getSendText();
-                client.sendTCP(chatMessage);
-            }
         });
-        // This listener is called when the chat window is closed.
-        chatFrame.setCloseListener(new Runnable() {
-
-            public void run() {
-                client.stop();
-            }
-        });
-        chatFrame.setVisible(true);
-
-        // We'll do the connect on a new thread so the ChatFrame can show a progress bar.
-        // Connecting to localhost is usually so fast you won't see the progress bar.
-        new Thread("Connect") {
-
-            public void run() {
-                try {
-                    client.connect(5000, host, Network.port);
-                    // Server communication after connection can go here, or in Listener#connected().
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    System.exit(1);
-                }
-            }
-        }.start();
-    }
-
-    static private class ChatFrame extends JFrame {
-
-        CardLayout cardLayout;
-        JProgressBar progressBar;
-        //JList messageList; Comented out to try JTextFrame
-        JTextPane messageList;
-        JTextField sendText;
-        JButton sendButton;
-        JList nameList;
-
-        public ChatFrame(String host) {
-            super("Chat Client");
-            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            setSize(640, 200);
-            setLocationRelativeTo(null);
-
-            Container contentPane = getContentPane();
-            cardLayout = new CardLayout();
-            contentPane.setLayout(cardLayout);
-            {
-                JPanel panel = new JPanel(new BorderLayout());
-                contentPane.add(panel, "progress");
-                panel.add(new JLabel("Connecting to " + host + "..."));
-                {
-                    panel.add(progressBar = new JProgressBar(), BorderLayout.SOUTH);
-                    progressBar.setIndeterminate(true);
-                }
-            }
-            {
-                JPanel panel = new JPanel(new BorderLayout());
-                contentPane.add(panel, "chat");
-                {
-                    JPanel topPanel = new JPanel(new GridLayout(1, 2));
-                    panel.add(topPanel);
-                    {
-                        //topPanel.add(new JScrollPane(messageList = new JList()));
-                        //messageList.setModel(new DefaultListModel());
-                        topPanel.add(new JScrollPane(messageList = new JTextPane()));
-                    }
-                    {
-                        topPanel.add(new JScrollPane(nameList = new JList()));
-                        nameList.setModel(new DefaultListModel());
-                    }
-                    DefaultListSelectionModel disableSelections = new DefaultListSelectionModel() {
-
-                        public void setSelectionInterval(int index0, int index1) {
-                        }
-                    };
-                    //messageList.setSelectionModel(disableSelections);
-                    nameList.setSelectionModel(disableSelections);
-                }
-                {
-                    JPanel bottomPanel = new JPanel(new GridBagLayout());
-                    panel.add(bottomPanel, BorderLayout.SOUTH);
-                    bottomPanel.add(sendText = new JTextField(), new GridBagConstraints(0, 0, 1, 1, 1, 0, GridBagConstraints.CENTER,
-                            GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-                    bottomPanel.add(sendButton = new JButton("Send"), new GridBagConstraints(1, 0, 1, 1, 0, 0,
-                            GridBagConstraints.CENTER, 0, new Insets(0, 0, 0, 0), 0, 0));
-                }
-            }
-
-            sendText.addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    sendButton.doClick();
-                }
-            });
-        }
-
-        public void setSendListener(final Runnable listener) {
-            sendButton.addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent evt) {
-                    if (getSendText().length() == 0) {
-                        return;
-                    }
-                    listener.run();
-                    sendText.setText("");
-                    sendText.requestFocus();
-                }
-            });
-        }
-
-        public void setCloseListener(final Runnable listener) {
-            addWindowListener(new WindowAdapter() {
-
-                public void windowClosed(WindowEvent evt) {
-                    listener.run();
-                }
-
-                public void windowActivated(WindowEvent evt) {
-                    sendText.requestFocus();
-                }
-            });
-        }
-
-        public String getSendText() {
-            return sendText.getText().trim();
-        }
-
-        public void setNames(final String[] names) {
-            // This listener is run on the client's update thread, which was started by client.start().
-            // We must be careful to only interact with Swing components on the Swing event thread.
-            EventQueue.invokeLater(new Runnable() {
-
-                public void run() {
-                    cardLayout.show(getContentPane(), "chat");
-                    DefaultListModel model = (DefaultListModel) nameList.getModel();
-                    model.removeAllElements();
-                    for (String name : names) {
-                        model.addElement(name);
-                    }
-                }
-            });
-        }
-
-        public void addMessage(final String message) {
-            EventQueue.invokeLater(new Runnable() {
-
-                @Override
-                public void run() {
-//                    DefaultListModel model = (DefaultListModel) messageList.getModel();
-//                    model.addElement(message);
-//                    messageList.ensureIndexIsVisible(model.size() - 1);
-                    Document doc = messageList.getDocument();
-                    try {
-                        if (doc.getLength() > 0) {
-                            doc.insertString(doc.getLength(), '\n' + message, null);
-                        } else {
-                            doc.insertString(doc.getLength(), message, null);
-                        }
-                    } catch (BadLocationException ex) {
-                        Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    messageList.setDocument(doc);
-                }
-            });
-        }
     }
 }
---
+++ src/org/cakemix/chat/ChatFrame.java
@@ -0,0 +1,251 @@
+package org.cakemix.chat;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
+public class ChatFrame extends JFrame {
+
+        CardLayout cardLayout;
+        JProgressBar progressBar;
+        //JList messageList; Comented out to try JTextFrame
+        JTextPane messageList;
+        JTextField sendText;
+        JButton sendButton, newConnection, toggleList;
+        JList nameList;
+
+        public ChatFrame(String host) {
+            super("Chat Client");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setLocationRelativeTo(null);
+
+            Container contentPane = getContentPane();
+            cardLayout = new CardLayout();
+            contentPane.setLayout(cardLayout);
+            {
+                JPanel panel = new JPanel(new BorderLayout());
+                contentPane.add(panel, "progress");
+                panel.add(new JLabel("Connecting to " + host + "..."));
+                {
+                    panel.add(progressBar = new JProgressBar(), BorderLayout.SOUTH);
+                    progressBar.setIndeterminate(true);
+                }
+            }
+            {
+                buildUI(contentPane);
+                //super pack cus we want the window to fit the gubbins
+                super.pack();
+            }
+            
+            // when enter is pressed in the send text box
+            // send the text by doing the send button click
+            sendText.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    sendButton.doClick();
+                }
+            });
+        }
+
+        /*
+         * override for pack
+         * does not resize the window ;)
+         */
+        @Override
+        public void pack() {
+            Dimension newSize = getSize();
+            super.pack();
+            setSize(newSize);
+        }
+
+        private void buildUI(Container contentPane) {
+            //create a panel to chuck everything on
+            JPanel panel = new JPanel(new BorderLayout());
+            //add panel to the main frame
+            contentPane.add(panel, "chat");
+            {
+                // create the panel to hold the top componants
+                JPanel topPanel = new JPanel();
+                // create the layout for the panel
+                GroupLayout layout = new GroupLayout(topPanel);
+                layout.setAutoCreateGaps(true);
+                layout.setAutoCreateContainerGaps(true);
+                // tell the panel to use the layout
+                topPanel.setLayout(layout);
+
+                // create the panes
+                // messages
+                JScrollPane messagePane = new JScrollPane(
+                        messageList = new JTextPane());
+                //set prefered and min sizes
+                // aparently a bit of a no no...
+                //messagePane.setPreferredSize(new Dimension(640, 480));
+                //messagePane.setMinimumSize(new Dimension(320, 240));
+
+                // user list
+                final JScrollPane usersPane = new JScrollPane(nameList = new JList());
+                //set prefered width of user list
+                //usersPane.setPreferredSize(new Dimension(64, 64));
+//                usersPane.setMinimumSize(new Dimension(320, 240));
+                //set list porperties
+                nameList.setModel(new DefaultListModel());
+                DefaultListSelectionModel disableSelections = new DefaultListSelectionModel() {
+                    public void setSelectionInterval(int index0, int index1) {
+                    }
+                };
+                nameList.setSelectionModel(disableSelections);
+
+                // create the funky buttons
+                newConnection = new JButton("New Connection");
+                toggleList = new JButton("Hide Userlist");
+
+
+
+                //set action for toggleList
+                toggleList.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (usersPane.isVisible()) {
+                            usersPane.setVisible(false);
+                            pack();
+                        } else {
+                            usersPane.setVisible(true);
+                            pack();
+                        }
+                    }
+                });
+
+                //create input controls
+                sendText = new JTextField();
+                sendButton = new JButton("Send");
+
+                // create the group
+                layout.setHonorsVisibility(true);
+                layout.setHorizontalGroup(layout.createParallelGroup()
+                        //buttons at top (change this for a menu)
+                        .addGroup(layout.createSequentialGroup()
+                        .addComponent(newConnection)
+                        .addComponent(toggleList))
+                        //main focus (left list && right chat)
+                        .addGroup(layout.createSequentialGroup()
+                        .addComponent(usersPane, 64, 96, 128)
+                        .addComponent(messagePane, 64, 320, Short.MAX_VALUE))
+                        //input at the bottom
+                        .addGroup(layout.createSequentialGroup()
+                        .addComponent(sendText)
+                        .addComponent(sendButton)));
+                layout.setVerticalGroup(layout.createSequentialGroup()
+                        //buttons at top (change this for a menu)                        
+                        .addGroup(layout.createParallelGroup()
+                        .addComponent(newConnection)
+                        .addComponent(toggleList))
+                        //main focus (left list && right chat)                        
+                        .addGroup(layout.createParallelGroup()
+                        .addComponent(messagePane)
+                        .addComponent(usersPane))
+                        //input at the bottom
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(sendText)
+                        .addComponent(sendButton)));
+
+
+                // add the panel to the main panel
+                panel.add(topPanel);
+            }
+
+        }
+
+        protected void createNewConnection(final Runnable listener) {
+            newConnection.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    listener.run();
+                }
+            });
+        }
+
+        public void setSendListener(final Runnable listener) {
+            sendButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    if (getSendText().length() == 0) {
+                        return;
+                    }
+                    listener.run();
+                    sendText.setText("");
+                    sendText.requestFocus();
+                }
+            });
+        }
+
+        public void setCloseListener(final Runnable listener) {
+            addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent evt) {
+                    listener.run();
+                }
+
+                @Override
+                public void windowActivated(WindowEvent evt) {
+                    sendText.requestFocus();
+                }
+            });
+        }
+
+        public String getSendText() {
+            return sendText.getText().trim();
+        }
+
+        public void setNames(final String[] names) {
+            // This listener is run on the client's update thread, 
+            // which was started by client.start().
+            // We must be careful to only interact with Swing 
+            // components on the Swing event thread.
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    cardLayout.show(getContentPane(), "chat");
+                    DefaultListModel model = (DefaultListModel) nameList.getModel();
+                    model.removeAllElements();
+                    for (String name : names) {
+                        model.addElement(name);
+                    }
+                }
+            });
+        }
+
+        public void addMessage(final String message) {
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    //update list rather than text frame
+//                    DefaultListModel model = (DefaultListModel) messageList.getModel();
+//                    model.addElement(message);
+//                    messageList.ensureIndexIsVisible(model.size() - 1);
+                    Document doc = messageList.getDocument();
+                    try {
+                        if (doc.getLength() > 0) {
+                            doc.insertString(doc.getLength(), '\n' + message, null);
+                        } else {
+                            doc.insertString(doc.getLength(), message, null);
+                        }
+                    } catch (BadLocationException ex) {
+                        Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    messageList.setDocument(doc);
+                }
+            });
+        }
+    }
